89d9ea71e64c263b011bc08808814b8209256420
Vsevolod <cerneavschivsevolod@gmail.com>
2021-05-01 19:28:09 +0200

    Header Fatto

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="dist/css/master.css">
+</head>
+<body>
+<header>
+  <div class="header-top">
+    <div class="logo-privacy">
+      <div class="">
+        <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+      </div>
+      <div>Privacy e termini</div>
+    </div>
+      <div class="">
+      <img src="./dist\img\cattura.png" alt="">
+      <i class="far fa-user-circle"></i>
+    </div>
+  </div>
+  <div class="header-bottom">
+  <ul>
+    <li>Introduzione</li>
+    <li>Norme sula privacy</li>
+    <li>Tecnologie</li>
+    <li>Domande freqventi</li>
+
+  </ul>
+  </div>
+
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
